360f84772a40106f6ac90d08eb8bac764d88b412
Arman Eskandari <71627457+ArmanEskandari@users.noreply.github.com>
2022-01-17 09:05:35 +0330

    removed unnecessary fragment tag

--- src/components/my-form/index.tsx
+++ src/components/my-form/index.tsx
@@ -159,142 +159,134 @@ class MyForm extends React.Component<any, MyState> {
 
     render() {
         return (
-            <>
-                <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-                    <div className="max-w-md w-full space-y-8">
-                        <div>
-                            <img
-                                src={logo}
-                                className="App-logo mx-auto h-12 w-64 h-60"
-                                alt="logo"
-                            />
-                            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                                Sign in to your account
-                            </h2>
-                        </div>
-                        <form
-                            className="mt-8 space-y-6"
-                            onSubmit={(e) => {
-                                e.preventDefault()
-                                this.state.formValid &&
-                                    this.state.emailValid &&
-                                    this.state.passwordValid &&
-                                    this.handleSubmitForm()
-                            }}
-                        >
-                            <input
-                                type="hidden"
-                                name="remember"
-                                defaultValue="true"
-                            />
-                            <div className="rounded-md shadow-sm -space-y-px">
-                                <div className="my-5">
-                                    <label
-                                        htmlFor="email-address"
-                                        className="sr-only"
-                                    >
-                                        Email address
-                                    </label>
-                                    <input
-                                        id="email-address"
-                                        name="email"
-                                        type="email"
-                                        autoComplete="email"
-                                        required
-                                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                        placeholder="Email address"
-                                        value={this.state.email}
-                                        onChange={this.handleUserInput}
-                                    />
-                                    {this.state.email.length > 0 && (
-                                        <div className="panel panel-default">
-                                            <InputErrorRenderer
-                                                formErrors={
-                                                    this.state.formErrors
-                                                }
-                                            />
-                                        </div>
-                                    )}
-                                </div>
-                                <div className="my-5">
-                                    <label
-                                        htmlFor="password"
-                                        className="sr-only"
-                                    >
-                                        Password
-                                    </label>
-                                    <input
-                                        id="password"
-                                        name="password"
-                                        type="password"
-                                        autoComplete="current-password"
-                                        required
-                                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                        placeholder="Password"
-                                        value={this.state.password}
-                                        onChange={this.handleUserInput}
-                                    />
-                                    {this.state.password.length > 0 && (
-                                        <div className="panel panel-default">
-                                            <InputErrorRenderer
-                                                formErrors={
-                                                    this.state.formErrors
-                                                        .password
-                                                }
-                                            />
-                                        </div>
-                                    )}
-                                </div>
-                            </div>
-
-                            <div className="flex items-center justify-between">
-                                <div className="flex items-center">
-                                    <input
-                                        id="remember-me"
-                                        name="remember-me"
-                                        type="checkbox"
-                                        onClick={this.showPassword}
-                                        className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                                    />
-                                    <label
-                                        htmlFor="remember-me"
-                                        className="ml-2 block text-sm text-gray-900"
-                                    >
-                                        Show password
-                                    </label>
-                                </div>
-
-                                <div className="text-sm">
-                                    <a
-                                        href="#"
-                                        className="font-medium text-indigo-600 hover:text-indigo-500"
-                                    >
-                                        Forgot your password?
-                                    </a>
-                                </div>
-                            </div>
-
-                            <div>
-                                <button
-                                    type="submit"
-                                    disabled={!this.state.formValid}
-                                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                >
-                                    {!this.state.formValid && (
-                                        <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                                            <LockClosedIcon
-                                                className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                                                aria-hidden="true"
-                                            />
-                                        </span>
-                                    )}
-                                    Sign in
-                                </button>
-                            </div>
-                        </form>
+            <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="max-w-md w-full space-y-8">
+                    <div>
+                        <img
+                            src={logo}
+                            className="App-logo mx-auto h-12 w-64 h-60"
+                            alt="logo"
+                        />
+                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                            Sign in to your account
+                        </h2>
                     </div>
+                    <form
+                        className="mt-8 space-y-6"
+                        onSubmit={(e) => {
+                            e.preventDefault()
+                            this.state.formValid &&
+                                this.state.emailValid &&
+                                this.state.passwordValid &&
+                                this.handleSubmitForm()
+                        }}
+                    >
+                        <input
+                            type="hidden"
+                            name="remember"
+                            defaultValue="true"
+                        />
+                        <div className="rounded-md shadow-sm -space-y-px">
+                            <div className="my-5">
+                                <label
+                                    htmlFor="email-address"
+                                    className="sr-only"
+                                >
+                                    Email address
+                                </label>
+                                <input
+                                    id="email-address"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    placeholder="Email address"
+                                    value={this.state.email}
+                                    onChange={this.handleUserInput}
+                                />
+                                {this.state.email.length > 0 && (
+                                    <div className="panel panel-default">
+                                        <InputErrorRenderer
+                                            formErrors={this.state.formErrors}
+                                        />
+                                    </div>
+                                )}
+                            </div>
+                            <div className="my-5">
+                                <label htmlFor="password" className="sr-only">
+                                    Password
+                                </label>
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    placeholder="Password"
+                                    value={this.state.password}
+                                    onChange={this.handleUserInput}
+                                />
+                                {this.state.password.length > 0 && (
+                                    <div className="panel panel-default">
+                                        <InputErrorRenderer
+                                            formErrors={
+                                                this.state.formErrors.password
+                                            }
+                                        />
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center">
+                                <input
+                                    id="remember-me"
+                                    name="remember-me"
+                                    type="checkbox"
+                                    onClick={this.showPassword}
+                                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                                />
+                                <label
+                                    htmlFor="remember-me"
+                                    className="ml-2 block text-sm text-gray-900"
+                                >
+                                    Show password
+                                </label>
+                            </div>
+
+                            <div className="text-sm">
+                                <a
+                                    href="#"
+                                    className="font-medium text-indigo-600 hover:text-indigo-500"
+                                >
+                                    Forgot your password?
+                                </a>
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                disabled={!this.state.formValid}
+                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            >
+                                {!this.state.formValid && (
+                                    <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                                        <LockClosedIcon
+                                            className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                                            aria-hidden="true"
+                                        />
+                                    </span>
+                                )}
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
                 </div>
-            </>
+            </div>
         )
     }
 }
